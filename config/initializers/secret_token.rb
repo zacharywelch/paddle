e044e0dcd4f20776842be1b1c76e4060e06aea06
zwelchcb <Zachary.Welch@careerbuilder.com>
2014-11-25 21:59:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paddle::Application.config.secret_key_base = 'afa92e28c5bb035975a2c42c9f695ad096c0c6d880c63ad67c0482104dfc4e2cd5eb692b7176caf6a8c3dad1b7bd280bcb38f1e52d161f31b0e14ffe8f525481'
