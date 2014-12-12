df1ac391edc0ee7919b194547d5a054b1d9c9e10
Paul Tashman <paul.tashman@careerbuilder.com>
2014-12-11 21:03:37 -0500

    Adds match form

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'uglifier', '2.1.1'
 gem 'coffee-rails', '4.0.1'
 gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
-
+gem 'byebug'
 group :development do
   gem 'annotate', '2.5.0'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Paddle::Application.routes.draw do
   resources :users
   resources :sessions,      only: [:new, :create, :destroy]
+  resources :matches,       only: [:new, :create]
   root to: 'pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
