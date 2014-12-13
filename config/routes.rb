d903408ee76202a3c5d688d9027e4936d79565f8
zwelchcb <Zachary.Welch@careerbuilder.com>
2014-12-12 23:32:27 -0500

    set root to users index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Paddle::Application.routes.draw do
   resources :users
   resources :sessions,      only: [:new, :create, :destroy]
   resources :matches,       only: [:new, :create]
-  root to: 'sessions#new'
+  root to: 'users#index'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
