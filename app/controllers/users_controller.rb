02c0bf88470321618ac7707aa6625f479f9323ef
zwelchcb <Zachary.Welch@careerbuilder.com>
2014-12-15 16:51:17 -0500

    update nickname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password,
+      params.require(:user).permit(:first_name, :last_name, :email, :nickname, :password,
                                    :password_confirmation)
     end
 
